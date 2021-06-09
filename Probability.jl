405c3777953ef396cd07447caac0aeab51b0bbd0
Dimitar Vanguelov <dvanguelov@gmail.com>
2021-06-09 12:57:55 -0400

    Day 3

--- Probability.jl
+++ Probability.jl
@@ -14,7 +14,7 @@ using PlutoUI
 md"""
 # A Concrete Introduction to Probability (using Julia)
 
-### Preface
+### Translator's Note
 
 *This lesson was originally [authored by Peter Norvig for the Python programming language](https://github.com/norvig/pytudes/blob/master/ipynb/Probability.ipynb). While this notebook aims to recreate that lesson as faithfully as possible, in content and in spirit -- a lot of the text is almost verbatim one-to-one with the original as written by Dr. Norvig -- certain liberties need to be taken when translating code from one language to another. The two languages are syntactically similar enough, yet certain concepts don't translate neatly from one to the other. In any case, I hope you enjoy reading (and interacting!) with this notebook for I find probability, Pluto, and Julia all quite beautiful.*
 
@@ -76,14 +76,14 @@ end;
 md"""
 ## Warm-up Problem: Die Roll
 
-What's the probability of rolling an even number with a single six-sided fair die? Mathematicians traditionally use a single capital letter to denote a sample space; we'll use D for the die:
+What's the probability of rolling an even number with a single six-sided fair die? Mathematicians traditionally use a single capital letter to denote a sample space; we'll use `D` for the die:
 """
 
 # ╔═╡ 66d84a3e-2270-4370-b721-f0ce727f2069
-D = Set([1, 2, 3, 4, 5, 6])
+D = Set([1, 2, 3, 4, 5, 6]);
 
 # ╔═╡ d46d3555-db5e-4463-b960-1523f17d69b6
-even = Set([2, 4, 6])
+even = Set([2, 4, 6]);
 
 # ╔═╡ 34215c3a-c740-4fb8-8c4c-2b09a0db17ee
 P(even, D)
@@ -94,10 +94,10 @@ Good to confirm what we already knew. We can explore some other events:
 """
 
 # ╔═╡ 876558b2-689f-4f68-bed4-46acb12cb9b2
-prime = Set([2, 3, 5, 7, 11, 13])
+prime = Set([2, 3, 5, 7, 11, 13]);
 
 # ╔═╡ b084aa98-a3a0-4718-a1a4-711a2b193884
-odd = Set([1, 3, 5, 7, 9, 11, 13])
+odd = Set([1, 3, 5, 7, 9, 11, 13]);
 
 # ╔═╡ 0e0af04e-1c08-4571-b984-93be3250b373
 P(odd, D)
@@ -116,25 +116,29 @@ Consider dealing a hand of five playing cards. An individual card has a rank and
 """
 
 # ╔═╡ 9179759a-5c1e-403d-a8b9-3a2235702bda
-begin
-	suits = "♥♠♦♣"
-	ranks = "AKQJT98765432"
-end;
+suits = "♥♠♦♣";
+
+# ╔═╡ 047c5adc-c89c-4949-bc44-43f4aecf0658
+ranks = "AKQJT98765432";
 
 # ╔═╡ e40381c6-761e-4f8c-8b20-d9a2ed2516a2
-deck = [(r, s) for r in ranks, s in suits]
+deck = [(r,s) for r in ranks, s in suits]
+
+# ╔═╡ a3381527-72b8-4640-a13f-db3358bf8172
+md"Note that each card in `deck` is a *tuple* consisting of a rank and suit. We deviate from the Python version here since working with tuples is faster in Julia than with strings."
 
 # ╔═╡ 8c6b90ba-ceb7-4ca9-a05d-c67eb588f5a9
 length(deck)
 
 # ╔═╡ 9eeb67a8-a165-441d-93fc-f2668234bd2d
 md"""
-Now we want to define Hands as the sample space of all 5-card combinations from deck. The function i`combinations` does most of the work; we than concatenate each combination into a space-separated string:
+Now we want to define `Hands` as the sample space of all 5-card combinations from deck. The function `combinations` from the `Combinatorics.jl` package does most of the work; we than group each combination into a vector (1d array) of card tuples:
 """
 
 # ╔═╡ 324d8993-eb02-4cbc-aef7-10a07ed3b848
 function combos(items, n)
-	return Set(combinations(items, n))
+	c = combinations(items, n)
+	return Set(c)
 end;
 
 # ╔═╡ 1053da15-abf0-45bd-9781-7eb3bd69f77c
@@ -158,11 +162,16 @@ md"Now we can answer questions like the probability of being dealt a flush (5 ca
 # ╔═╡ c3de8c2d-ba7f-4ef7-a3e5-ceee6c79411c
 flush = collect(Hands)[length.(unique.([getfield.(collect(h), 2) for h in Hands])) .== 1] |> Set
 
+# or slightly cleaner
+
+# flush = begin
+# 	hands = collect(Hands)
+# 	idx = length.(unique.(getfield.(h, 2) for h in hands)) .== 1
+# 	Set(hands[idx])
+# end
+
 # ╔═╡ 4eef87b3-67d4-419d-964f-127fbabebda3
 P(flush, Hands)
-
-# ╔═╡ 5b0102b0-fb46-4a22-a995-3d823630456f
-
 
 # ╔═╡ 7316770c-f7da-47b5-b3d0-b7873c3eafa4
 note(text) = Markdown.MD(Markdown.Admonition("note", "Note", [text]));
@@ -176,7 +185,7 @@ note(md"""
 """)
 
 # ╔═╡ a4b79e42-87bc-4bb9-b990-517fd220d6cc
-note(md"*Experiment a bit in the cell above -- see what happens if you change the comma to a `for` in the array comprehension, or see what happens when you execute `deck[:]`, or wrap it in `Set()`, etc.*")
+note(md"*As a way of learning about arrays in Julia, experiment a bit in the cell above -- see what happens if you change the comma to a `for` in the array comprehension, or see what happens when you execute `deck[:]`, or wrap it in `Set()`, etc.*")
 
 # ╔═╡ 06a5b841-edfb-400f-9bc8-d6a36433a1ba
 html"""<style>
@@ -208,7 +217,9 @@ TableOfContents()
 # ╟─fffddca0-bd3b-470c-ba59-7b50f6d6f6cb
 # ╟─ccd786ef-d3a8-4eef-bc54-905b54ee8922
 # ╠═9179759a-5c1e-403d-a8b9-3a2235702bda
+# ╠═047c5adc-c89c-4949-bc44-43f4aecf0658
 # ╠═e40381c6-761e-4f8c-8b20-d9a2ed2516a2
+# ╟─a3381527-72b8-4640-a13f-db3358bf8172
 # ╟─a4b79e42-87bc-4bb9-b990-517fd220d6cc
 # ╠═8c6b90ba-ceb7-4ca9-a05d-c67eb588f5a9
 # ╟─9eeb67a8-a165-441d-93fc-f2668234bd2d
@@ -221,7 +232,6 @@ TableOfContents()
 # ╟─16d0d2dc-ebeb-4d87-b578-447324c466dd
 # ╠═c3de8c2d-ba7f-4ef7-a3e5-ceee6c79411c
 # ╠═4eef87b3-67d4-419d-964f-127fbabebda3
-# ╠═5b0102b0-fb46-4a22-a995-3d823630456f
 # ╟─d27d1867-e6ba-4fb1-8370-96403b786e00
 # ╟─7316770c-f7da-47b5-b3d0-b7873c3eafa4
 # ╟─06a5b841-edfb-400f-9bc8-d6a36433a1ba
